01b3123d909ddb85a1b4ff50ac5691ba4e8d48e5
Piotr Ste <pstefanski@gmail.com>
2019-12-07 00:26:17 +0100

    PlayerGenerator generate
    bedzie

--- core/src/stefan/PlayerGenerator.java
+++ core/src/stefan/PlayerGenerator.java
@@ -4,17 +4,28 @@ import assets.AssetLoader;
 import boost.GameObject;
 import boost.SpriteObject;
 
+import java.util.Random;
+
 public class PlayerGenerator {
+
+    static int  nHeads = 0,
+                nBodys = 0,
+                nLegs = 0;
+
     public static GameObject generate() {
-        int headId = 1;
         GameObject player = new GameObject();
-        GameObject head = AssetLoader.getAsset("head" + headId);
-        // TODO body
-        SpriteObject legs = AssetLoader.getAnimation("jade");
-        legs.chooseAnimation("run");
+        Random random = new Random();
+
+        SpriteObject head = AssetLoader.getAnimation("head" + random.nextInt(nHeads));
+        SpriteObject body = AssetLoader.getAnimation("body" + random.nextInt(nBodys));
+        SpriteObject legs = AssetLoader.getAnimation("legs" + random.nextInt(nLegs));
 
         player.addActor(legs);
+        player.addActor(body);
         player.addActor(head);
         return player;
     }
+
+
+
 }
